49a9aff6f26121c1c10b2fd31559a83df82f8292
Ben Danon <bendanon305@gmail.com>
2015-05-01 15:17:52 +0300

    Created CodeWriterUtils.py containing utility classes for CodeWriter

--- CodeWriter.py
+++ CodeWriter.py
@@ -1,48 +1,12 @@
 from Common import CommandType
+from CodeWriterUtils import Function
+from CodeWriterUtils import Label
+from CodeWriterUtils import StaticVariable
 
 #Constant definitions for bootstrap
 SP_INITIAL_VALUE = 256
 SP_POSITION = 0
 
-class Function:
-    def __init__(self, functionName, numArgs):
-        self.functionName = functionName
-        self.numArgs = numArgs
-
-    def getRetSymbol(self):
-        return "#{1}".format(self.functionName)
-
-    def getNumArgs(self):
-        return self.numArgs
-
-class Label:
-    def __init__(self, containingFunction, labelName):
-        self.containingFunction = containingFunction
-        self.labelName = labelName
-
-    def getUniqueLabelName(self):
-        return "{0}${1}".format(self.containingFunction, self.labelName)
-
-class StaticVariable:
-
-    currentOffsetFromStaticBase = 0
-
-    def __init__(self, containingFile, variableName):
-        self.containingFile = containingFile
-        self.variableName = variableName
-
-        #This is the variable allocation
-        self.address = staticProvidedBases["static"] + currentOffsetFromStaticBase
-
-        #Increment the current offset for the next allocation
-        currentOffsetFromStaticBase += 1 
-
-    def getUniqueSymbolName(self):
-        return "{0}.{1}".format(self.containingFile, self.variableName)
-
-    def getMemoryAddress(self): 
-        return self.address
-    
 '''
 Translates VM commands into Hack assembly code.
 '''
